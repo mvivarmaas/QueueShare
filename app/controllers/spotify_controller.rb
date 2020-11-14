8a8d62ed9bf5c0cf07ba3bb21ec29b6f60ef471b
Max Vivar-Maas <mvivarmaas@gmail.com>
2020-11-14 18:38:34 -0500

    Update HTML/CSS

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -1,5 +1,12 @@
 class SpotifyController < ApplicationController
   def callback
-
+    host = RSpotify::User.new(request.env['omniauth.auth'])
+    @is_nil = true
+    unless host.player.nil?
+      @is_nil = false
+      @is_playing = host.player.playing?
+      @currently_playing = host.player.currently_playing if @is_playing
+      @display_name = host.display_name
+    end
   end
 end
