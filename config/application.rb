9530e3ea6a53ef6912498ca61ba7ac2f79a1a588
Max Vivar-Maas <mvivarmaas@gmail.com>
2020-11-15 13:57:53 -0500

    remove dotenv

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,10 @@
 require_relative 'boot'
-require 'Dotenv'
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
+#Dotenv::Railtie.load
 RSpotify.authenticate(ENV["CLIENT_ID"], ENV["CLIENT_SECRET"])
 
 module QueueShare
