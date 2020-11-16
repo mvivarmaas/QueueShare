fac979ebab6276a506df0d380e7251f86ad9188b
Max Vivar-Maas <mvivarmaas@gmail.com>
2020-11-16 12:25:58 -0500

    Playlist now added to User's spotify library on click

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -3,18 +3,15 @@ class SpotifyController < ApplicationController
     @host = RSpotify::User.new(request.env['omniauth.auth'])
     seeds = @host.top_tracks(limit: 5)
     @tracks = RSpotify::Recommendations.generate(seed_tracks: seeds.map(&:id)).tracks
+    playlist = @host.create_playlist!('A Surprise From QueueShare', public: false)
+    playlist.add_tracks!(@tracks)
     @is_nil = true
     unless @host.player.nil?
       @is_nil = false
       @is_playing = @host.player.playing?
-      @currently_playing = @ost.player.currently_playing if @is_playing
+      @currently_playing = @host.player.currently_playing if @is_playing
       @display_name = @host.display_name
     end
-  end
-
-  def playlist
-    playlist = @host.create_playlist!('QueueShare')
-    playlist.add_tracks!(@tracks)
   end
 end
 
