1c34d6b1d6011e5ca412ccbe623f63ee24f822db
Max Vivar-Maas <mvivarmaas@gmail.com>
2020-11-15 22:06:19 -0500

    Have playlists added to user profile

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -1,19 +1,20 @@
 class SpotifyController < ApplicationController
   def callback
-    host = RSpotify::User.new(request.env['omniauth.auth'])
-    seeds = host.top_tracks(limit: 5)
+    @host = RSpotify::User.new(request.env['omniauth.auth'])
+    seeds = @host.top_tracks(limit: 5)
     @tracks = RSpotify::Recommendations.generate(seed_tracks: seeds.map(&:id)).tracks
     @is_nil = true
-    unless host.player.nil?
+    unless @host.player.nil?
       @is_nil = false
-      @is_playing = host.player.playing?
+      @is_playing = @host.player.playing?
       @currently_playing = @ost.player.currently_playing if @is_playing
-      @display_name = host.display_name
+      @display_name = @host.display_name
     end
   end
 
   def playlist
-
+    playlist = @host.create_playlist!('QueueShare')
+    playlist.add_tracks!(@tracks)
   end
 end
 
